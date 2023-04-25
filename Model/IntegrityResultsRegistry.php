4f405ef83166bd995af4a295d3ddb8729a9d0507
pykettk <pykettk@gmail.com>
2023-04-26 00:36:03 +0100

    Decouple Package Failure Extraction Logic from Cron Logic

--- Model/IntegrityResultsRegistry.php
+++ Model/IntegrityResultsRegistry.php
@@ -7,14 +7,39 @@ declare(strict_types=1);
 
 namespace Element119\SansecComposerIntegrityChecker\Model;
 
+use Element119\SansecComposerIntegrityChecker\Enum\IntegrityCheckerArrayKeys;
+use Element119\SansecComposerIntegrityChecker\Scope\Config;
 use Exception;
 use Hyva\Admin\Api\HyvaGridArrayProviderInterface;
+use Magento\Framework\Data\Collection\AbstractDb;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Flag;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\Serialize\Serializer\Serialize;
 
 class IntegrityResultsRegistry extends Flag implements HyvaGridArrayProviderInterface
 {
     protected $_flagCode = 'sansec_composer_integrity_checker_results';
+
+    private Config $moduleConfig;
+
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        Config $moduleConfig,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = [],
+        Json $json = null,
+        Serialize $serialize = null
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data, $json, $serialize);
+
+        $this->moduleConfig = $moduleConfig;
+    }
 
     public function getHyvaGridData(): array
     {
@@ -38,5 +63,19 @@ class IntegrityResultsRegistry extends Flag implements HyvaGridArrayProviderInte
         $this->setFlagData($data);
 
         return $this->save();
+    }
+
+    public function getFailedMatches(array $packages = []): array
+    {
+        $failures = [];
+        $threshold = $this->moduleConfig->getSansecComposerIntegrityMatchThreshold();
+
+        foreach ($packages ?: $this->getLastResults() as $package) {
+            if ((int)$package[IntegrityCheckerArrayKeys::Percentage->value] < $threshold) {
+                $failures[] = $package;
+            }
+        }
+
+        return $failures;
     }
 }
