d0d394ef94c99232c74d1ae12839042b937ea39d
pykettk <pykettk@gmail.com>
2023-04-27 16:39:16 +0100

    Update Flag Data on Admin Grid Run Action Success

--- Controller/Adminhtml/Scan/Run.php
+++ Controller/Adminhtml/Scan/Run.php
@@ -7,30 +7,42 @@ declare(strict_types=1);
 
 namespace Element119\SansecComposerIntegrityChecker\Controller\Adminhtml\Scan;
 
+use Element119\SansecComposerIntegrityChecker\Model\IntegrityResultsRegistry;
 use Element119\SansecComposerIntegrityChecker\Service\Scanner;
+use Exception;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\FileSystemException;
 
 class Run extends Action
 {
     public const ADMIN_RESOURCE = 'Element119_SansecComposerIntegrityChecker::run';
 
+    private IntegrityResultsRegistry $integrityResultsRegistry;
     private Scanner $scanner;
 
     public function __construct(
         Context $context,
+        IntegrityResultsRegistry $integrityResultsRegistry,
         Scanner $scanner
     ) {
         parent::__construct($context);
 
+        $this->integrityResultsRegistry = $integrityResultsRegistry;
         $this->scanner = $scanner;
     }
 
-    public function execute()
+    /**
+     * @throws FileSystemException
+     * @throws Exception
+     */
+    public function execute(): ResultInterface
     {
         $results = $this->scanner->scan();
 
         if ($results) {
+            $this->integrityResultsRegistry->setResults($results);
             $this->messageManager->addSuccessMessage(__('Scan complete.'));
         } else {
             $this->messageManager->addErrorMessage(__('Could not scan packages.'));
