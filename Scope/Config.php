2ab66c9b93b4efdbb0c791fb26e5b948fcd00b40
pykettk <pykettk@gmail.com>
2023-05-17 22:33:38 +0100

    Create New Admin Config Options

--- Scope/Config.php
+++ Scope/Config.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Element119\SansecComposerIntegrityChecker\Scope;
 
 use Element119\SansecComposerIntegrityChecker\Block\Adminhtml\Form\Field\ComposerIntegrityNotificationEmailRecipients;
+use Element119\SansecComposerIntegrityChecker\Block\Adminhtml\Form\Field\IgnoredPackages;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Serialize\SerializerInterface;
 
@@ -17,6 +18,14 @@ class Config
     private const XML_PATH_EMAIL_ENABLED = 'system/sansec_composer_integrity_checker/scan_email_notification_enable';
     private const XML_PATH_EMAIL_THRESHOLD = 'system/sansec_composer_integrity_checker/scan_failure_threshold';
     private const XML_PATH_EMAIL_RECIPIENTS = 'system/sansec_composer_integrity_checker/scan_error_email_recipient';
+    private const XML_PATH_IGNORE_LIST_ENABLE = 'system/sansec_composer_integrity_checker/package_ignore_list_enable';
+    private const XML_PATH_IGNORED_PACKAGES = 'system/sansec_composer_integrity_checker/ignored_packages';
+    private const XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_ADMIN_GRID =
+        'system/sansec_composer_integrity_checker/remove_ignored_packages_from_admin_grid';
+    private const XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_ADMIN_NOTIFICATION =
+        'system/sansec_composer_integrity_checker/remove_ignored_packages_from_admin_notif';
+    private const XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_EMAIL =
+        'system/sansec_composer_integrity_checker/remove_ignored_packages_from_emails';
 
     public function __construct(
         private readonly ScopeConfigInterface $scopeConfig,
@@ -52,5 +61,41 @@ class Config
         }
 
         return $recipients;
+    }
+
+    public function isIgnoreListEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_IGNORE_LIST_ENABLE);
+    }
+
+    public function getIgnoredPackageList(): array
+    {
+        $ignoredPackages = [];
+        $ignoredPackagesConfig = $this->scopeConfig->getValue(self::XML_PATH_IGNORED_PACKAGES) ?? [];
+
+        if (!is_array($ignoredPackagesConfig)) {
+            $ignoredPackagesConfig = $this->serializer->unserialize($ignoredPackagesConfig);
+        }
+
+        foreach ($ignoredPackagesConfig as $ignoredPackage) {
+            $ignoredPackages[] = $ignoredPackage[IgnoredPackages::PACKAGE];
+        }
+
+        return $ignoredPackages;
+    }
+
+    public function shouldRemoveIgnoredPackagesFromAdminGrid(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_ADMIN_GRID);
+    }
+
+    public function shouldRemoveIgnoredPackagesFromAdminNotification(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_ADMIN_NOTIFICATION);
+    }
+
+    public function shouldRemoveIgnoredPackagesFromEmail(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_REMOVE_IGNORED_PACKAGES_FROM_EMAIL);
     }
 }
